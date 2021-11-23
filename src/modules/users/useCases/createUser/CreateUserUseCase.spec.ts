fbc055be273ff0bf969c232393760022f85ad066
Amaro <amaro@solutiontrue.com.br>
2021-11-23 16:59:28 -0300

    added part of the test

--- src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -1,8 +1,42 @@
+import { UsersRepositoryInMemory } from "../../repositories/In-memory/UsersRepositoryInMemory";
 import { CreateUserUseCase } from "./CreateUserUseCase"
 
 describe("Create User", () => {
 
-  it("should be able to create a new user", () => {
-    const createUser = new CreateUserUseCase
+  it("should be able to create a new user", async () => {
+    const user = {
+      id: "b67705e4-9d01-4e6b-9fb1-ec5b758726eb",
+      name: "Administrador",
+      email: "admin@admin.com",
+      phone: 123456789,
+      cpf: 32268300900,
+      ra: 123456789,
+      profile: "Administrador",
+      active: true
+    };
+
+    await createUserUseCase.execute({
+      id: "b67705e4-9d01-4e6b-9fb1-ec5b758726eb",
+      name: "Administrador",
+      email: "admin@admin.com",
+      phone: 123456789,
+      cpf: 32268300900,
+      ra: 123456789,
+      profile: "Administrador",
+      active: true
+    });
+
+    const UserCreated = await UsersRepositoryInMemory.findByUser({  
+      id: "b67705e4-9d01-4e6b-9fb1-ec5b758726eb",
+      name: "Administrador",
+      email: "admin@admin.com",
+      phone: 123456789,
+      cpf: 32268300900,
+      ra: 123456789,
+      profile: "Administrador",
+      active: true
+    });
+
+    console.log(userCreated)
   })
 })
